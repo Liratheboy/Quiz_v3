6451c33c3699303ae57f02aaf8b141af1569a852
João Paulo Lira <76487819+Liratheboy@users.noreply.github.com>
2024-01-08 11:08:37 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Quiz sobre programação!
+
+Nesta terceira versão, pretendo adicionar tela inicial e final.
+
+Além das telas inicias, 
---
+++ js.js
@@ -0,0 +1,76 @@
+const pergunta1 = document.getElementById('container_p1');
+const pergunta2 = document.getElementById('container_p2');
+const pergunta3 = document.getElementById('container_p3');
+const pergunta4 = document.getElementById('container_p4');
+const pergunta5 = document.getElementById('container_p5');
+const pergunta6 = document.getElementById('container_p6');
+const pergunta7 = document.getElementById('container_p7');
+const pergunta8 = document.getElementById('container_p8');
+const pergunta9 = document.getElementById('container_p9');
+const pergunta10 = document.getElementById('container_p10');
+
+function altcerta() {
+  alert('Alternativa correta!')
+} 
+function alterrada(){
+  alert('Alternativa errada!')
+}
+
+function mudarPergunta1(){  
+
+  pergunta1.style.display = "none";
+  pergunta2.style.display = "block";
+}
+
+function mudarPergunta2(){  
+  
+  pergunta2.style.display = "none";
+  pergunta3.style.display = "block";
+}
+
+function mudarPergunta3(){  
+
+  pergunta3.style.display = "none";
+  pergunta4.style.display = "block";
+}
+
+function mudarPergunta4(){  
+
+  pergunta4.style.display = "none";
+  pergunta5.style.display = "block";
+}
+
+function mudarPergunta5(){  
+
+  pergunta5.style.display = "none";
+  pergunta6.style.display = "block";
+}
+
+function mudarPergunta6(){  
+
+  pergunta6.style.display = "none";
+  pergunta7.style.display = "block";
+}
+
+function mudarPergunta7(){  
+
+  pergunta7.style.display = "none";
+  pergunta8.style.display = "block";
+}
+
+function mudarPergunta8(){  
+
+  pergunta8.style.display = "none";
+  pergunta9.style.display = "block";
+}
+
+function mudarPergunta9(){  
+
+  pergunta9.style.display = "none";
+  pergunta10.style.display = "block";
+}
+
+function mudarPergunta10(){  
+
+  
+}
